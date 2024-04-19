15b3be3b7725db731d3c5ab09ca9d6eed3901c56
normaljosh <josh@normal.dev>
2024-04-19 14:51:34 -0400

    make UI nice

---
+++ static/components/export-steps.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card rounded="0" flat>
+    <v-window v-model="step" show-arrows>
+      <v-window-item v-for="n in steps" :key="`card-${n.id}`" :value="n.id">
+        <h3 class="text-h6">{{ n.title }}</h3>
+        <br>
+        <v-sheet>
+          <div class="text-body-1">
+            {{ n.description }}
+          </div>
+          <v-img :height="600" cover :src="n.img">          
+          </v-img>
+        </v-sheet>
+      </v-window-item>
+    </v-window>
+  </v-card>
+</template>
+
+
+<script>
+const defaultDesc = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.`
+export default {
+  data: () => ({
+    steps: [
+      { id: 1, title: "Step 1", description: "Lorem Ipsum", img: "images/step-1.png" },
+      { id: 2, title: "Step 2", description: defaultDesc, img: "images/step-2.png" },
+      { id: 3, title: "Step 3", description: defaultDesc, img: "images/step-3.png" },
+      { id: 4, title: "Step 4", description: defaultDesc, img: "images/step-4.png" },
+      { id: 5, title: "Step 5", description: defaultDesc, img: "images/step-5.png" },
+      { id: 6, title: "Step 6", description: defaultDesc, img: "images/step-6.png" },
+      { id: 7, title: "Step 7", description: defaultDesc, img: "images/step-7.png" }
+    ],
+    step: 1
+  }),
+
+  methods: {
+    next() {
+      this.step = this.step + 1 > this.steps.length
+        ? 1
+        : this.step + 1
+    },
+    prev() {
+      this.step = this.step - 1 <= 0
+        ? this.steps.length
+        : this.step - 1
+    },
+  },
+}
+</script>
---
+++ static/components/stepper.vue
@@ -0,0 +1,190 @@
+<template>
+  <v-stepper v-model="step" :items="items" show-actions editable>
+    <template v-slot:item.1>
+      <export-steps />
+    </template>
+
+    <template v-slot:item.2>
+      <h3 class="text-h6">Select Exported file to upload</h3>
+      <br>
+      <v-file-input ref="fileupload" clearable @change="handleFileUpload($event)" accept=".csv"
+        label="ALC Export" chips :loading="isLoading"></v-file-input>
+    </template>
+
+    <template v-slot:item.3>
+      <template v-if="!uploadState.status">
+        <div style="display: flex; justify-content: space-between;">
+          <h3 class="text-h6">Preview Data</h3>
+          <v-btn variant="elevated" color="#5865f2" @click="importData" :loading="isLoading">Import</v-btn>
+        </div>
+        <br>
+        <ag-grid-vue :getRowStyle="getRowStyle" :columnDefs="columnDefs" :rowData="rowData"
+          style="height: 600px; width: 100%; flex-grow: 1" class="ag-theme-alpine"></ag-grid-vue>
+      </template>
+      <template v-else>
+        <v-card style="margin: 50px;" class="mx-auto" max-width="344">
+          <v-card-text>
+            <p class="text-h4 font-weight-black" style="color: green;">Import Success</p>
+            <div style="display: flex; align-items: center; margin-top: 10px; gap: 8px;">
+              <div class="text-h5">
+                Total Points Updated:
+              </div>
+              <div class="text-h4">{{ uploadState.updatedCount }}</div>
+            </div>
+            <v-btn style="margin-top: 24px;" @click="uploadAnother">Upoad Another</v-btn>
+          </v-card-text>
+        </v-card>
+      </template>
+    </template>
+  </v-stepper>
+</template>
+
+<script>
+import exportSteps from './export-steps.vue';
+
+var sdk = new nf(window.location.origin);
+
+const createPointQuery = (point) => ({
+  and: [
+    {
+      field: {
+        property: "device_id",
+        text: point?.["Device ID"].split(":")?.[1],
+      },
+    },
+    {
+      field: {
+        property: "object_id",
+        text: point?.["Object ID"].replace(":", "."),
+      },
+    },
+  ],
+});
+
+export default {
+  components: {
+    ["export-steps"]: exportSteps,
+  },
+  data: () => ({
+    filename: "",
+    file: "",
+    isLoading: false,
+    content: [],
+    parsed: false,
+    step: 1,
+    items: [
+      'Export from ALC',
+      'Upload Export',
+      'Import to NF',
+    ],
+    columnDefs: [
+      { field: "action", sortable: true, sort: "desc" },
+      { field: "Location", sortable: true },
+      { field: "Control Program", sortable: true },
+      { field: "Name", sortable: true },
+      { field: "Type", sortable: true },
+      { field: "Object ID", sortable: true },
+      { field: "Device ID", sortable: true },
+      { field: "Object Name", sortable: true },
+      { field: "Offset / Polarity", sortable: true },
+      { field: "Exp:Num", sortable: true },
+      { field: "I/O Type", sortable: true },
+    ],
+    rowData: [],
+    uploadState: {}
+  }),
+  watch: {
+    async content() {
+      this.isLoading = true;
+      await this.processData();
+      this.isLoading = false;
+    }
+  },
+  methods: {
+    chunk: (arr, size) =>
+      Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+        arr.slice(i * size, i * size + size),
+      ),
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+      this.parseFile();
+    },
+    parseFile() {
+      Papa.parse(this.file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+          this.content = results;
+          this.parsed = true;
+        }.bind(this),
+      });
+    },
+    async importData() {
+      this.isLoading = true;
+      const updates = this.rowData.filter(r => r.action === 'Update')
+        .map(r => ({ layer: 'alc', uuid: r.uuid, attrs: { name: r.Name, location: r.Location, control_program: r["Control Program"] } }))
+
+      const chunks = this.chunk(updates, 100);
+
+      for (const points of chunks) {
+        await sdk.updatePoints(points);
+      }
+      this.isLoading = false;
+      this.resetUploadState()
+      this.uploadState = { status: 'success', updatedCount: updates.length }
+    },
+    async processData() {
+      if (!this.parsed) return;
+      const groups = this.chunk(this.content.data, 100);
+      for (const group of groups) {
+        const points = await sdk.getPoints({
+          or: group.map(createPointQuery),
+        });
+
+        group.forEach((aclPoint) => {
+          const point = points.find((p) => {
+            return (
+              p.attrs.device_id === aclPoint?.["Device ID"].split(":")?.[1] &&
+              p.attrs.object_id ===
+              aclPoint?.["Object ID"].replace(":", ".").toLowerCase()
+            );
+          })
+          if (point) {
+            aclPoint.action = "Update";
+            aclPoint.uuid = point.uuid;
+          } else {
+            aclPoint.action = "Skip (point not defined)";
+          }
+        });
+      }
+      this.rowData = groups.flatMap(g => g);
+    },
+    resetUploadState() {
+      this.$refs.fileupload.reset()
+      this.file = "";
+      this.content = [];
+      this.parsed = false;
+      this.rowData = []
+    },
+    uploadAnother() {
+      this.resetUploadState();
+      this.step = 2;
+      this.uploadState = {};
+    },
+    getRowStyle: params => {
+      console.log(params.node)
+      if (params.node.data.action === 'Update') {
+        return { background: '#90EE90' };
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.stepper {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+</style>
