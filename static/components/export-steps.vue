84f0688df0bccc50ea8e2997b8e4f331304644a2
normaljosh <josh@normal.dev>
2024-04-22 15:49:34 -0400

    improve export steps

--- static/components/export-steps.vue
+++ static/components/export-steps.vue
@@ -1,14 +1,14 @@
 <template>
   <v-card rounded="0" flat>
     <v-window v-model="step" show-arrows>
-      <v-window-item v-for="n in steps" :key="`card-${n.id}`" :value="n.id">
+      <v-window-item v-for="n in steps" :key="`card-${n.id}`" :value="n.id" eager>
         <h3 class="text-h6">{{ n.title }}</h3>
-        <br>
         <v-sheet>
           <div class="text-body-1">
             {{ n.description }}
           </div>
-          <v-img :height="600" cover :src="n.img">          
+          <br>
+          <v-img max-height="600" :src="n.img" eager>   
           </v-img>
         </v-sheet>
       </v-window-item>
@@ -28,13 +28,9 @@ const defaultDesc = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
 export default {
   data: () => ({
     steps: [
-      { id: 1, title: "Step 1", description: "Lorem Ipsum", img: "images/step-1.png" },
-      { id: 2, title: "Step 2", description: defaultDesc, img: "images/step-2.png" },
-      { id: 3, title: "Step 3", description: defaultDesc, img: "images/step-3.png" },
-      { id: 4, title: "Step 4", description: defaultDesc, img: "images/step-4.png" },
-      { id: 5, title: "Step 5", description: defaultDesc, img: "images/step-5.png" },
-      { id: 6, title: "Step 6", description: defaultDesc, img: "images/step-6.png" },
-      { id: 7, title: "Step 7", description: defaultDesc, img: "images/step-7.png" }
+      { id: 1, title: "Step 1", description: "From the reports menu, open the Equipment submenu and select Point List", img: "images/step-1.png" },
+      { id: 2, title: "Step 2", description: "Click to the Options tab, and ensure the “Type” and “Object ID | Device ID | Object Name” columns are checked, and that the “BACnet points” checkbox under the “Include Only” heading are checked. Then hit “Run”", img: 'images/step-2.png' },
+      { id: 3, title: "Step 3", description: "After the report has completed, click the “CSV” button. Save the resulting download to your hard drive, and upload it in the next step.", img: "images/step-3.png" },
     ],
     step: 1
   }),
