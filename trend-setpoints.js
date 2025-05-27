ad02559460e4079c964171fa0acb028799e5642c
Stephen Dawson-Haggerty <sdhags@gmail.com>
2025-05-27 20:51:33 +0000

    Add initial script with firmware dumping and creation of basic points

---
+++ trend-setpoints.js
@@ -0,0 +1,44 @@
+const NormalSdk = require("@normalframework/applications-sdk");
+
+/**
+ * Invoke hook function
+ * @param {NormalSdk.InvokeParams} params
+ * @returns {NormalSdk.InvokeResult}
+ */
+module.exports = async ({points, sdk, update, args}) => {
+  //console.log(points)
+  for (let i = 0; i < points.length; i++) {
+  let res = await sdk.http.post("/api/v2/bacnet/confirmed-service", {
+            "device_address": {
+                "device_id": points[i].attrs.device_id,
+            }, "request": {
+                "atomic_read_file": {
+                    "file_identifier": {
+                        "object_type": "OBJECT_TYPE_FILE",
+                        "instance": points[i].attrs.instance,
+                    },
+                    "stream_access": {
+                        "file_start_position": points[i].attrs.offset,
+                        "requested_octet_count": 4,
+                    },
+                }
+            }
+        }, {
+            timeout: 10000,
+        })
+        let data = Buffer.from(res.data.ack.atomicReadFile.streamAccess.fileData, 'base64')
+        let value = data.readFloatBE(0)
+        sdk.logEvent(`${points[i].uuid}: ${value}}`)
+        res = await sdk.http.post("/api/v1/point/data", {
+            "uuid": points[i].uuid,
+            "values": [
+                {
+                    "ts": new Date().toISOString(),
+                    "real": value,
+                },
+            ],
+            "is_async": true,
+        })
+        console.log(res.data)
+  }
+};
---
+++ write-setpoints.js
@@ -0,0 +1,10 @@
+const NormalSdk = require("@normalframework/applications-sdk");
+
+/**
+ * Invoke hook function
+ * @param {NormalSdk.InvokeParams} params
+ * @returns {NormalSdk.InvokeResult}
+ */
+module.exports = async ({points, sdk, update, args}) => {
+  
+};
